f90a51d7117c60ae093b394c160eebe5a774581b
smithB <bradley.carlyle.smith@gmail.com>
2020-03-01 17:39:26 -0600

    Big commit Big commit

--- stimuli/exp_text.js
+++ stimuli/exp_text.js
@@ -1,6 +1,6 @@
 var welcome_text = "Welcome to the experiment. This experiment will take no longer than 30 minutes. Press any key to begin.";
 
-var instruction_text = "<p>In this experiment, you will be listening to sounds made by babies between the ages of 6-36 months. Your task will be to try and identify characteristics of the baby making those sounds (age, sex, familial language). You will respond by selecting the corresponding button when presented. Please make these judgements based on your initial impressions. You must listen to all audio clips before responding and if there are multiple audio clips they are guaranteed to be from the same child.</p>"+"<p>Press any key to begin.</p>";
+var instruction_text = "<p>In this experiment, you will be listening to sounds made by babies between the ages of 0-36 months. Your task will be to try and identify characteristics of the baby making those sounds (age, sex, familial language). You will respond by selecting the corresponding button when presented. Please make these judgements based on your initial impressions. You must listen to all audio clips before responding and if there are multiple audio clips they are guaranteed to be from the same child.</p>"+"<p>Press any key to begin.</p>";
 
 var language_choices = ["English","Non-English"];
 
@@ -34,7 +34,7 @@ var Engl_first_lang_prompt = "Is English your first language?";
 
 var Engl_first_lang_opt = ["Yes", "No"];
 
-var Know_corp_lang_prompt = "Do you know any of the following languages, even a little? Spanish, Tsimane', Yeli-Dnye, Tseltal Mayan, or Quechua?"; //Yêlí-Dnye characters don't show up
+var Know_corp_lang_prompt = "Do you know any of the following languages, even a little? (Select all that apply)"; // Spanish, Tsimane', Yeli-Dnye, Tseltal Mayan, or Quechua?"; //Yêlí-Dnye characters don't show up
 
 var Know_corp_lang_opt = ["Spanish","Tsimane","Yeli-Dnye", "Tseltal Mayan", "Quechua"];
 
