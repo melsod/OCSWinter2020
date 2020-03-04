f8bdaf58d069667edfc8d28c4c3e5103d721e983
smitty0015 <bradley.carlyle.smith@gmail.com>
2020-03-03 22:57:51 -0600

    Update exp_text.js
    update wording

--- stimuli/exp_text.js
+++ stimuli/exp_text.js
@@ -24,7 +24,7 @@ var sex_prompt = "<p>What is the baby's Sex?</p>";
 
 var debrief_text = "<p>Thanks for participating! If you would like more information about this experiment then please visit <b>https://osf.io/2a6b4</b> </p>";
 
-var childcare_text = "Estimate how many months you have worked in childcare? (answer '0' if none)";
+var childcare_text = "In the last 5 years, estimate how many months you have worked/volunteered in childcare or an area with significant exposure to infants? (answer '0' if none)";
 
 var caregiver_text = "In the last 5 years, estimate how many months you have you been the primary caregiver for a child under the age of 3 years (answer '0' if none)"; //different than on prereg
 
