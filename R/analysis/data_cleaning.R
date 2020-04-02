3aefc1f107f07b5397e8091122725c3b9a1bd663
smitty0015 <bradley.carlyle.smith@gmail.com>
2020-04-01 23:34:31 -0500

    looking into data cleaning

--- R/analysis/data_cleaning.R
+++ R/analysis/data_cleaning.R
@@ -1,14 +1,10 @@
-# needs work and commenting
-
 # install.packages("jsonlite")
 library("jsonlite")
 
-#data <- unlist(jsonlite::fromJSON("https://raw.githubusercontent.com/smitty0015/OCSWinter2020/master/R/analysis/short_exp_test_data.json"), recursive = FALSE, use.names = TRUE)
-data <- unlist(jsonlite::fromJSON("./data/data.json"), recursive = FALSE, use.names = TRUE)
-
-
 # install.packages("plyr")
 library("plyr")
+
+data <- unlist(jsonlite::fromJSON("data/final_data.json"), recursive = FALSE, use.names = TRUE)
 
 data <- rbind.fill(data)
 
@@ -27,6 +23,8 @@ extract_subject_data <- function(d){
                mean(d$correct[d$phase == "Language"], na.rm=TRUE),
                mean(d$correct[d$phase == "Age"], na.rm=TRUE),
                max(d$time_elapsed) / 60000,
+               min(d$time_elapsed) / 60000, # BCS added
+               (max(d$time_elapsed)-min(d$time_elapsed)) / 60000, # BCS added
                as.numeric(extract_json("Childcare", survey_data)),
                as.numeric(extract_json("Caregiver", survey_data)),
                as.numeric(extract_json("Par_Age", survey_data)),
@@ -38,18 +36,26 @@ extract_subject_data <- function(d){
                as.character(extract_json("Engl_first_lang", survey_data)),
                as.character(paste0(extract_json("Know_corp_lang", survey_data))),
                as.character(extract_json("monolingual", survey_data)),
+               as.numeric(sum(d$phase == "Attention_Check", na.rm=TRUE)),
+               as.numeric(var(d$button_pressed[d$phase == "Sex"] , na.rm=TRUE)),
+               as.numeric(var(d$button_pressed[d$phase == "Language"] , na.rm=TRUE)),
+               as.numeric(var(d$button_pressed[d$phase == "Age"] , na.rm=TRUE)),
                stringsAsFactors = FALSE)
     
 }
 
-tidy_data <- data.frame(matrix(0, length(unique(data$subject)), 16))
+tidy_data <- data.frame(matrix(0, length(unique(data$subject)), 22))
 
 for(i in 1:length(unique(data$subject))){
     tidy_data[i, ] <- extract_subject_data(data[data$subject == unique(data$subject)[i], ])
 }
 
-colnames(tidy_data) <- c("suject_ID", "p_cor_sex", "p_cor_lang", "p_cor_age", "time_mins", "childcare", "caregiver", 
+# BCS add some time measures
+colnames(tidy_data) <- c("subject_ID", "p_cor_sex", "p_cor_lang", "p_cor_age", "time_mins","fin_consent_mins","consent_to_fin_mins", "childcare", "caregiver", 
                          "age", "gender", "gender_text", "country", "country_text", "hearing", 
-                         "eng_first", "know_corp_lang", "monolingual")
+                         "eng_first", "know_corp_lang", "monolingual", "n_attention_checks",
+                         "var_sex", "var_lang", "var_age")
+
+tidy_data$know_corp_lang[tidy_data$know_corp_lang == "list()"] <- "None"
 
 write.csv(tidy_data, "./data/cleaned_data.csv", row.names = FALSE)
