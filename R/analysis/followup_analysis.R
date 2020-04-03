cd161cf298e47940f0cac1b2276051e7a21e7908
smitty0015 <bradley.carlyle.smith@gmail.com>
2020-04-03 17:02:39 -0500

    Update followup_analysis.R

--- R/analysis/followup_analysis.R
+++ R/analysis/followup_analysis.R
@@ -1,51 +1,75 @@
-#Logistic Data pull out by Brad & Matt 
+#································Get Trial-by-Trial Data ··············································
 
 #install.packages("jsonlite")
 library("jsonlite")
 
-#data <- unlist(jsonlite::fromJSON("https://raw.githubusercontent.com/smitty0015/OCSWinter2020/master/R/analysis/short_exp_test_data.json"), recursive = FALSE, use.names = TRUE)
+# pull in data (should work assuming you have cloned the GitHub Repo)
 data <- unlist(jsonlite::fromJSON("./data/final_data.json"), recursive = FALSE, use.names = TRUE)
-
 
 # install.packages("plyr")
 library("plyr")
 
+# collapse a JSON level
 data <- rbind.fill(data)
 
-
+# funciton to pull JSON data
 extract_json <- function(key, survey_data){
   return(survey_data[[max(grep(key, survey_data))]])
 }
 
+# create empty dataframe
 tidy_data <- data.frame(matrix(0, length(unique(data$subject))*34, 15))
+
+# set count
 count<-1
+
+# for each subject in the data
 for(i in 1:length(unique(data$subject))){
+  # get the data for just 1 participant
   sub_data <- data[data$subject == unique(data$subject)[i],]
+  # get that participants survey data
   survey_data <- sub_data$responses[sub_data$phase == "Survey"]
   survey_data <- survey_data[! is.na(survey_data)]
+  # extract JSON data
   survey_data <- sapply(survey_data, fromJSON)
+  # get subject ID code
   subject<-sub_data$subject[1]
+  # get childcare experience in months
   childcare<-as.numeric(extract_json("Childcare", survey_data))
-  #if(is.na(childcare)==TRUE){as.numeric(strsplit(extract_json("Childcare", survey_data),split = " ")[[1]][1])} # subject typed "XX months" this with catch that
+  
+  # program required a numerical response but allowed responses like "12 months" (as long as it started with a number)
+  # this section extracts the data if that happened
+  
+  # if trying to make childcare numeric forced an NA then
   if(is.na(childcare)==TRUE){
-    ###
-    #print(extract_json("Childcare", survey_data)[[1]])
-    #print(strsplit(extract_json("Childcare", survey_data)[[1]],split = " ")[[1]])
+    # get the string value and split it on spaces
     childcare_txt<-strsplit(extract_json("Childcare", survey_data)[[1]],split = " ")
-    #print(childcare)
-    ###
+    
+    # if there is only 1 value after the split (meaning no spaces)
     if(length(childcare_txt[[1]])==1){
-      #print(strsplit(extract_json("Childcare", survey_data)[[1]],split = "-"))
+      # then split on "-" (they probably wrote "4-6")
       childcare_txt<-strsplit(extract_json("Childcare", survey_data)[[1]],split = "-")
+      # then take the mean of the first and last number as our estimate
       childcare<-mean(c(as.numeric(childcare_txt[[1]][1]),as.numeric(childcare_txt[[1]][-1])))
+    
+    # or else
     }else{
+      
+      # if the first letter of the second "word" is y then we'll assume they wrote something like "2 years"
       if(tolower(substring(text = childcare_txt[[1]][2],first = 1,last = 1))=="y"){
+        # then multiply the numerical value by 12 and use that value
         childcare<-(12)*as.numeric(childcare_txt[[1]][1])
+      
+      # or else
       }else{
+        # assume they wrote something like "12 months" and just use the numerical value
         childcare<-as.numeric(childcare_txt[[1]][1])
       }
     }
   }
+  # end section accounting for bad data entry for childcare
+  
+  # Do the same steps as above (for Childcare) for the variable Caregiving
   caregiver<-as.numeric(extract_json("Caregiver", survey_data)[[1]])
   if(is.na(caregiver)==TRUE){
     caregiver_txt<-strsplit(extract_json("Caregiver", survey_data)[[1]],split = " ")
@@ -60,17 +84,32 @@ for(i in 1:length(unique(data$subject))){
       }
     }
   }
+  # end section accoutn for bad data entry for caregiving
+  
+  # get participants age
   par_age<-as.numeric(extract_json("Par_Age", survey_data))
+  # get participant's gender
   par_gen<-as.character(extract_json("Gender_Q", survey_data)[[1]])
+  # get the text the participant chose to write in the "Other gender" box
   par_gen_other<-as.character(extract_json("Gender_Q", survey_data)[[2]])
+  # get the participant's country of residence
   par_country<-as.character(extract_json("Country_Q", survey_data)[[1]])
+  # get the text that the aprticipant chose to write int he "Other Country" box
   par_country_other<-as.character(extract_json("Country_Q", survey_data)[[2]])
+  # get whether the participant has normal hearing
   hearing<-as.character(extract_json("Normal_hearing", survey_data))
+  # get whether the participant has normal hearing
   engl_first<-as.character(extract_json("Engl_first_lang", survey_data))
+  # get whether the participant knows any of the corpus languages
   know_corp<-as.character(paste0(extract_json("Know_corp_lang", survey_data)))
+  # get whether the participant is monolingual
   monolingual<-as.character(extract_json("monolingual", survey_data))
+  
+  # for each data point (for the one participant we have singled out)
   for(j in 1:nrow(sub_data)){
+    # if this is an experimental trial
     if(sub_data$correct[j] %in% c(0,1)){
+      # then update the dataset we are creating with all the data (otherwise do nothing)
       tidy_data[count,] <- data.frame(subject,
                                       sub_data$correct[j],
                                       sub_data$phase[j],
@@ -87,12 +126,13 @@ for(i in 1:length(unique(data$subject))){
                                       know_corp,
                                       monolingual,
                                       stringsAsFactors = FALSE)
+      # updat the count
       count<-count+1
     }
   }
-  #tidy_data[i, ] <- extract_subject_data(data[data$subject == unique(data$subject)[i], ])
 }
 
+# set the variable names
 colnames(tidy_data) <- c("subject_ID", "correct", "phase", "time_ellapsed_mins", "childcare", "caregiver", 
                          "age", "gender", "gender_text", "country", "country_text", "hearing", 
                          "eng_first", "know_corp_lang", "monolingual")
@@ -100,7 +140,12 @@ colnames(tidy_data) <- c("subject_ID", "correct", "phase", "time_ellapsed_mins",
 
 #································Eliminate excluded participants ··············································
 
-
+#install.packages("readr")
+library(readr)
+# get datafile that includes participant exclusions (created by data_cleaning.R)
+w_exclusions_data <- read_csv("data/w_exclusions_data.csv")
+# only include the data from participants who pass exclusion criteria
+tidy_data<-subset(tidy_data,tidy_data$subject_ID %in% w_exclusions_data$subject_ID)
 
 
 #································Follow Up Analysis ··············································
