dd958a324aa06d16b2c8e25ef8a77b158a91ef31
BradleySmithC <bradley.carlyle.smith@gmail.com>
2022-01-13 11:42:16 -0600

    updateFigures_fixCaregivingAnalysis

--- R/analysis/create_figures.R
+++ R/analysis/create_figures.R
@@ -25,20 +25,20 @@ hist_sex<-ggplot(d, aes(p_cor_sex)) +
   geom_vline(xintercept = mean(d$p_cor_sex), linetype="solid",  color = "red", size=1.5) + # red line for average level of responses
   xlab("Proportion Correct for Sex Judgement") + # x-axis label
   ylab("Count") + # y-axis label
-  labs(title = "Histogram for Sex Judgements", # graph title
-       # below is the caption with a wrapper to make a new line every 100 characters, mean and sd inserted
-       caption = wrapper(paste0("*Figure 1*. X-axis represents the propotion of correct responses across the 10 experimental trials. Y-axis represents the number of participants with that propotion of correct responses. Mean proportion correct was ",
-                                round(mean(d$p_cor_sex),2),
-                                " (*SD* = ",
-                                round(sd(d$p_cor_sex), 2),
-                                ")."), 
-                         80))+
+  # labs(title = "Histogram for Sex Judgements", # graph title
+  #      # below is the caption with a wrapper to make a new line every 100 characters, mean and sd inserted
+  #      caption = wrapper(paste0("*Figure 1*. X-axis represents the propotion of correct responses across the 10 experimental trials. Y-axis represents the number of participants with that propotion of correct responses. Mean proportion correct was ",
+  #                               round(mean(d$p_cor_sex),2),
+  #                               " (*SD* = ",
+  #                               round(sd(d$p_cor_sex), 2),
+  #                               ")."), 
+  #                        80))+
   theme_bw()+ # use black and white theme
-  theme(plot.caption = ggtext::element_markdown(hjust = 0, size = 12, family = "Times New Roman"), axis.title = element_text(size = 14), panel.grid.major = element_blank(),panel.grid.minor = element_blank())
+  theme(plot.caption = ggtext::element_markdown(hjust = 0, size = 12, family = "Times New Roman"), axis.title = element_text(size = 14), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), plot.margin = unit(c(1,1,1,1),"lines"))
 
 # save histogram to file
 #png("paper/figures/hist_sex.png") # make file
-jpeg("paper/figures/hist_sex.jpeg") # make file
+jpeg("paper/figures/hist_sex.jpeg", quality = 100, res = 300, width = 1400, height = 1400) # make file
 print(hist_sex) # put histogram into the file
 dev.off() #turn off a dev tool so that the save completes
 
@@ -47,20 +47,20 @@ hist_language<-ggplot(d, aes(p_cor_lang)) + geom_histogram(bins = 10) +
   geom_vline(xintercept = 0.5, linetype="solid",  color = "darkgrey", size=1.5) +
   geom_vline(xintercept = mean(d$p_cor_lang), linetype="solid",  color = "red", size=1.5) +
   xlab("Proportion Correct for Language Judgement") + ylab("Count") +
-  labs(title = "Histogram for Language Judgements", 
-       caption = wrapper(paste0("*Figure 2*. X-axis represents the propotion of correct responses across the 12 experimental trials. Y-axis represents the number of participants with that propotion of correct responses. Mean proportion correct was ",
-                                round(mean(d$p_cor_lang),2),
-                                " (*SD* = ",
-                                round(sd(d$p_cor_lang), 2),
-                                ")."),
-                         80))+
+  # labs(title = "Histogram for Language Judgements", 
+  #      caption = wrapper(paste0("*Figure 2*. X-axis represents the propotion of correct responses across the 12 experimental trials. Y-axis represents the number of participants with that propotion of correct responses. Mean proportion correct was ",
+  #                               round(mean(d$p_cor_lang),2),
+  #                               " (*SD* = ",
+  #                               round(sd(d$p_cor_lang), 2),
+  #                               ")."),
+  #                        80))+
   theme_bw()+
-  theme(plot.caption = ggtext::element_markdown(hjust = 0, size = 12), axis.title = element_text(size = 14), panel.grid.major = element_blank(),panel.grid.minor = element_blank())
+  theme(plot.caption = ggtext::element_markdown(hjust = 0, size = 12), axis.title = element_text(size = 14), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), plot.margin = unit(c(1,1,1,1),"lines"))
 
 
 # save histogram to file
 #png("paper/figures/hist_language.png")
-jpeg("paper/figures/hist_language.jpeg")
+jpeg("paper/figures/hist_language.jpeg", quality = 100, res = 300, width = 1400, height = 1400)
 print(hist_language)
 dev.off()
 
@@ -69,18 +69,18 @@ hist_age<-ggplot(d, aes(p_cor_age)) + geom_histogram(bins = 10) +
   geom_vline(xintercept = (1/3), linetype="solid",  color = "darkgrey", size=1.5) +
   geom_vline(xintercept = mean(d$p_cor_age), linetype="solid",  color = "red", size=1.5) +
   xlab("Proportion Correct for Age Judgement") + ylab("Count") +
-  labs(title = "Histogram for Age Judgements", 
-       caption = wrapper(paste0("*Figure 3*. X-axis represents the propotion of correct responses across the 12 experimental trials. Y-axis represents the number of participants with that propotion of correct responses. Mean proportion correct was ",
-                                round(mean(d$p_cor_age),2),
-                                " (*SD* = ",
-                                round(sd(d$p_cor_age), 2),
-                                ")."), 
-                         80))+
+  # labs(title = "Histogram for Age Judgements", 
+  #      caption = wrapper(paste0("*Figure 3*. X-axis represents the propotion of correct responses across the 12 experimental trials. Y-axis represents the number of participants with that propotion of correct responses. Mean proportion correct was ",
+  #                               round(mean(d$p_cor_age),2),
+  #                               " (*SD* = ",
+  #                               round(sd(d$p_cor_age), 2),
+  #                               ")."), 
+  #                        80))+
   theme_bw()+
-  theme(plot.caption = ggtext::element_markdown(hjust = 0, size = 12), axis.title = element_text(size = 14), panel.grid.major = element_blank(),panel.grid.minor = element_blank())
+  theme(plot.caption = ggtext::element_markdown(hjust = 0, size = 12), axis.title = element_text(size = 14), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), plot.margin = unit(c(1,1,1,1),"lines"))
 
 # save histogram to file
 #png("paper/figures/hist_age.png")
-jpeg("paper/figures/hist_age.jpeg")
+jpeg("paper/figures/hist_age.jpeg", quality = 100, res = 300, width = 1400, height = 1400)
 print(hist_age)
 dev.off()
